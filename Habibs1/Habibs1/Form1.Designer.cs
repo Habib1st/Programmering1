e21abd793932a0cad7d265542c74cbc6dd85e6f5
Habib Allah Al Berotiy <0510HAAY@gapp.uddevalla.se>
2022-08-25 11:12:20 +0200

    Add a button

--- Habibs1/Habibs1/Form1.Designer.cs
+++ Habibs1/Habibs1/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BTNGIttmannen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTNGIttmannen
+            // 
+            this.BTNGIttmannen.Location = new System.Drawing.Point(209, 120);
+            this.BTNGIttmannen.Name = "BTNGIttmannen";
+            this.BTNGIttmannen.Size = new System.Drawing.Size(140, 79);
+            this.BTNGIttmannen.TabIndex = 0;
+            this.BTNGIttmannen.Text = "Gittmannen";
+            this.BTNGIttmannen.UseVisualStyleBackColor = true;
+            this.BTNGIttmannen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(622, 367);
+            this.Controls.Add(this.BTNGIttmannen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BTNGIttmannen;
     }
 }
 
