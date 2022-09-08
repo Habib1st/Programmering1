9138c2c555f4364eda562712419d32f1b5da15b7
Habib Allah Al Berotiy <0510HAAY@gapp.uddevalla.se>
2022-09-08 11:36:50 +0200

    hur man omvandlar char till string.

---
+++ övning 3.7 Unicode/övning 3.7 Unicode/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace övning_3._7_Unicode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTecken = new System.Windows.Forms.Label();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.btnKör = new System.Windows.Forms.Button();
+            this.tbxTecken = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTecken
+            // 
+            this.lblTecken.AutoSize = true;
+            this.lblTecken.Location = new System.Drawing.Point(188, 74);
+            this.lblTecken.Name = "lblTecken";
+            this.lblTecken.Size = new System.Drawing.Size(61, 20);
+            this.lblTecken.TabIndex = 0;
+            this.lblTecken.Text = "Tecken";
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.Location = new System.Drawing.Point(366, 232);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(41, 20);
+            this.lblSvar.TabIndex = 1;
+            this.lblSvar.Text = "Svar";
+            // 
+            // btnKör
+            // 
+            this.btnKör.Location = new System.Drawing.Point(338, 301);
+            this.btnKör.Name = "btnKör";
+            this.btnKör.Size = new System.Drawing.Size(117, 48);
+            this.btnKör.TabIndex = 2;
+            this.btnKör.Text = "Kör";
+            this.btnKör.UseVisualStyleBackColor = true;
+            this.btnKör.Click += new System.EventHandler(this.btnKör_Click);
+            // 
+            // tbxTecken
+            // 
+            this.tbxTecken.Location = new System.Drawing.Point(355, 74);
+            this.tbxTecken.Name = "tbxTecken";
+            this.tbxTecken.Size = new System.Drawing.Size(100, 26);
+            this.tbxTecken.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbxTecken);
+            this.Controls.Add(this.btnKör);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.lblTecken);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTecken;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Button btnKör;
+        private System.Windows.Forms.TextBox tbxTecken;
+    }
+}
+
