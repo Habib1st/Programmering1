04ba9bb2b8d1d278985ed4917e9699251b2133a1
Habib Allah Al Berotiy <0510HAAY@gapp.uddevalla.se>
2023-05-10 14:29:51 +0200

    Klart med E

--- Slutprojekt Kryptering/Slutprojekt Kryptering/Form1.cs
+++ Slutprojekt Kryptering/Slutprojekt Kryptering/Form1.cs
@@ -25,20 +25,33 @@ namespace Slutprojekt_Kryptering
 
             foreach (char nummer in detSkrivna)
             {
-                if (Char.IsLetter(nummer) && nummer <= 90 && nummer<=122)
+                if (Char.IsLetter(nummer) && nummer >= 65 && nummer <= 90 )
                 {
                     char detEnkrypterade = (char)(((int)nummer + 3));
                     if (detEnkrypterade > 'Z')
                     {
                         detEnkrypterade = (char)(((int)nummer - 23));
+                        Console.WriteLine(detEnkrypterade);
+                       
+
                     }
+                    svaret += detEnkrypterade;
+
+                }
+
+                else if (Char.IsLetter(nummer) && nummer >= 97 && nummer <= 122)
+                {
+                    char detEnkrypterade = (char)(((int)nummer + 3));
                     if (detEnkrypterade > 'z')
                     {
                         detEnkrypterade = (char)(((int)nummer - 23));
-                    }
+                        Console.WriteLine(detEnkrypterade);
 
+                    }
                     svaret += detEnkrypterade;
                 }
+
+                
                
                 else
                 {
@@ -49,16 +62,63 @@ namespace Slutprojekt_Kryptering
             return svaret;
 
         }
+
+        public string Dekrypt3 (string detSkrivna)
+        {
+            string svaret = "";
+
+            foreach (char nummer in detSkrivna)
+            {
+                if (Char.IsLetter(nummer) && nummer >= 65 && nummer <= 90)
+                {
+                    char detEnkrypterade = (char)(((int)nummer - 3));
+                    if (detEnkrypterade < 'A')
+                    {
+                        detEnkrypterade = (char)(((int)nummer + 23));
+                        Console.WriteLine(detEnkrypterade);
+
+
+                    }
+                    svaret += detEnkrypterade;
+
+                }
+
+                else if (Char.IsLetter(nummer) && nummer >= 97 && nummer <= 122)
+                {
+                    char detEnkrypterade = (char)(((int)nummer - 3));
+                    if (detEnkrypterade < 'a')
+                    {
+                        detEnkrypterade = (char)(((int)nummer + 23));
+                        Console.WriteLine(detEnkrypterade);
+
+                    }
+                    svaret += detEnkrypterade;
+                }
+
+
+
+                else
+                {
+                    svaret += nummer;
+                }
+            }
+
+            return svaret;
+        }
         private void btnSkicka_Click(object sender, EventArgs e)
         {
+            btnDekrypt.Enabled = true;
             string kryptera = tbxKryptering.Text;
             tbxDekrypt.Text = Kryptering3(kryptera);
             
         }
 
+
         private void btnDekrypt_Click(object sender, EventArgs e)
         {
-            tbxDekrypt.Text = tbxKryptering.Text;
+            btnDekrypt.Enabled = false;
+            string dekrypt = tbxDekrypt.Text;
+            tbxDekrypt.Text = Dekrypt3(dekrypt);
         }
     }
 }
